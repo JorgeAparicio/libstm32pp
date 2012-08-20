eb822c10401be83c20be7eef62dd67fe828bb5b3
Jorge Aparicio <jorge.aparicio.r@gmail.com>
2012-08-20 05:31:12 -0500

    DEMO: Updated DMA USART demo.

--- demo/dma_usart_tx.hpp
+++ demo/dma_usart_tx.hpp
@@ -39,89 +39,75 @@ char msg[] = "Hello World!\n\r";
 
 int main()
 {
-  RCC::enableClocks<
-      rcc::ahb1enr::GPIOA
+  GPIOA::enableClock();
+
+  PA9::setAlternateFunction(gpio::afr::USART1_3);
+
+  PA9::setMode(gpio::moder::ALTERNATE);
+
+  PA10::setAlternateFunction(gpio::afr::USART1_3);
+
+  PA10::setMode(gpio::moder::ALTERNATE);
+
+  USART1::enableClock();
+
+  USART1::configure(
+      usart::cr1::rwu::RECEIVER_IN_ACTIVE_MODE,
+      usart::cr1::re::RECEIVER_ENABLED,
+      usart::cr1::te::TRANSMITTER_ENABLED,
+      usart::cr1::idleie::IDLE_INTERRUPT_DISABLED,
+      usart::cr1::rxneie::RXNE_ORE_INTERRUPT_DISABLED,
+      usart::cr1::tcie::TC_INTERRUPT_DISABLED,
+      usart::cr1::txeie::TXEIE_INTERRUPT_DISABLED,
+      usart::cr1::peie::PEIE_INTERRUPT_DISABLED,
+      usart::cr1::ps::EVEN_PARITY,
+      usart::cr1::pce::PARITY_CONTROL_DISABLED,
+      usart::cr1::wake::WAKE_ON_IDLE_LINE,
+      usart::cr1::m::START_8_DATA_N_STOP,
+      usart::cr1::ue::USART_ENABLED,
+      usart::cr1::over8::OVERSAMPLING_BY_16,
+      usart::cr2::stop::_1_STOP_BIT,
+      usart::cr3::eie::ERROR_INTERRUPT_DISABLED,
+      usart::cr3::hdsel::FULL_DUPLEX,
+      usart::cr3::dmar::RECEIVER_DMA_DISABLED,
+      usart::cr3::dmat::TRANSMITTER_DMA_ENABLED,
+      usart::cr3::rtse::RTS_HARDWARE_FLOW_DISABLED,
+      usart::cr3::ctse::CTS_HARDWARE_FLOW_DISABLED,
+      usart::cr3::ctsie::CTS_INTERRUPT_DISABLED,
+      usart::cr3::onebit::ONE_SAMPLE_BIT_METHOD);
+
+  USART1::setBaudRate<
+      9600 /* bps*/
   >();
 
-  PA9::setAlternateFunction<
-      gpio::registers::afr::states::USART1_3
-  >();
+  DMA2::enableClock();
 
-  PA9::setMode<
-      gpio::registers::moder::states::ALTERNATE
-  >();
-
-  PA10::setAlternateFunction<
-      gpio::registers::afr::states::USART1_3
-  >();
-
-  PA10::setMode<
-      gpio::registers::moder::states::ALTERNATE
-  >();
-
-  RCC::enableClocks<
-      rcc::apb2enr::USART1
-  >();
-
-  USART1::configure<
-      usart::registers::cr1::bits::rwu::states::RECEIVER_IN_ACTIVE_MODE,
-      usart::registers::cr1::bits::re::states::RECEIVER_ENABLED,
-      usart::registers::cr1::bits::te::states::TRANSMITTER_ENABLED,
-      usart::registers::cr1::bits::idleie::states::IDLE_INTERRUPT_DISABLED,
-      usart::registers::cr1::bits::rxneie::states::RXNE_ORE_INTERRUPT_DISABLED,
-      usart::registers::cr1::bits::tcie::states::TC_INTERRUPT_DISABLED,
-      usart::registers::cr1::bits::txeie::states::TXEIE_INTERRUPT_DISABLED,
-      usart::registers::cr1::bits::peie::states::PEIE_INTERRUPT_DISABLED,
-      usart::registers::cr1::bits::ps::states::EVEN_PARITY,
-      usart::registers::cr1::bits::pce::states::PARITY_CONTROL_DISABLED,
-      usart::registers::cr1::bits::wake::states::WAKE_ON_ADDRESS_MARK,
-      usart::registers::cr1::bits::m::states::START_8_DATA_N_STOP,
-      usart::registers::cr1::bits::ue::states::USART_ENABLED,
-      usart::registers::cr1::bits::over8::states::OVERSAMPLING_BY_16,
-      usart::registers::cr2::bits::stop::states::_1_STOP_BIT,
-      usart::registers::cr3::bits::eie::states::ERROR_INTERRUPT_DISABLED,
-      usart::registers::cr3::bits::hdsel::states::FULL_DUPLEX,
-      usart::registers::cr3::bits::dmar::states::RECEIVER_DMA_DISABLED,
-      usart::registers::cr3::bits::dmat::states::TRANSMITTER_DMA_ENABLED,
-      usart::registers::cr3::bits::rtse::states::RTS_HARDWARE_FLOW_DISABLED,
-      usart::registers::cr3::bits::ctse::states::CTS_HARDWARE_FLOW_DISABLED,
-      usart::registers::cr3::bits::ctsie::states::CTS_INTERRUPT_DISABLED,
-      usart::registers::cr3::bits::onebit::states::ONE_SAMPLE_BIT_METHOD
-  >();
-
-  USART1::setBaudRate(1667);  // 9600 bps @ 16MHZ APB2
-
-  RCC::enableClocks<
-    rcc::ahb1enr::DMA2
-  >();
-
-  DMA_U1TX::configure<
-      dma::stream::registers::cr::bits::dmeie::states::DIRECT_MODE_ERROR_INTERRUPT_DISABLED,
-      dma::stream::registers::cr::bits::teie::states::TRANSFER_ERROR_INTERRUPT_DISABLED,
-      dma::stream::registers::cr::bits::htie::states::HALF_TRANSFER_INTERRUPT_DISABLED,
-      dma::stream::registers::cr::bits::tcie::states::TRANSFER_COMPLETE_INTERRUPT_DISABLED,
-      dma::stream::registers::cr::bits::pfctrl::states::DMA_FLOW_CONTROLLER,
-      dma::stream::registers::cr::bits::dir::states::MEMORY_TO_PERIPHERAL,
-      dma::stream::registers::cr::bits::circ::states::CIRCULAR_MODE_DISABLED,
-      dma::stream::registers::cr::bits::pinc::states::PERIPHERAL_INCREMENT_MODE_DISABLED,
-      dma::stream::registers::cr::bits::minc::states::MEMORY_INCREMENT_MODE_ENABLED,
-      dma::stream::registers::cr::bits::psize::states::PERIPHERAL_SIZE_8BITS,
-      dma::stream::registers::cr::bits::msize::states::MEMORY_SIZE_8BITS,
-      dma::stream::registers::cr::bits::pincos::states::PERIPHERAL_INCREMENT_OFFSET_SIZE_32BITS,
-      dma::stream::registers::cr::bits::pl::states::PRIORITY_LEVEL_MEDIUM,
-      dma::stream::registers::cr::bits::dbm::states::DOUBLE_BUFFER_MODE_DISABLED,
-      dma::stream::registers::cr::bits::ct::states::CURRENT_TARGET_MEMORY_0,
-      dma::stream::registers::cr::bits::pburst::states::PERIPHERAL_BURST_TRANSFER_SINGLE,
-      dma::stream::registers::cr::bits::mburst::states::MEMORY_BURST_TRANSFER_SINGLE,
-      dma::stream::registers::cr::bits::chsel::states::CHANNEL_4
-  >();
+  DMA_U1TX::configure(
+      dma::stream::cr::dmeie::DIRECT_MODE_ERROR_INTERRUPT_DISABLED,
+      dma::stream::cr::teie::TRANSFER_ERROR_INTERRUPT_DISABLED,
+      dma::stream::cr::htie::HALF_TRANSFER_INTERRUPT_DISABLED,
+      dma::stream::cr::tcie::TRANSFER_COMPLETE_INTERRUPT_DISABLED,
+      dma::stream::cr::pfctrl::DMA_FLOW_CONTROLLER,
+      dma::stream::cr::dir::MEMORY_TO_PERIPHERAL,
+      dma::stream::cr::circ::CIRCULAR_MODE_ENABLED,
+      dma::stream::cr::pinc::PERIPHERAL_INCREMENT_MODE_DISABLED,
+      dma::stream::cr::minc::MEMORY_INCREMENT_MODE_ENABLED,
+      dma::stream::cr::psize::PERIPHERAL_SIZE_8BITS,
+      dma::stream::cr::msize::MEMORY_SIZE_8BITS,
+      dma::stream::cr::pincos::PERIPHERAL_INCREMENT_OFFSET_SIZE_32BITS,
+      dma::stream::cr::pl::PRIORITY_LEVEL_MEDIUM,
+      dma::stream::cr::dbm::DOUBLE_BUFFER_MODE_DISABLED,
+      dma::stream::cr::ct::CURRENT_TARGET_MEMORY_0,
+      dma::stream::cr::pburst::PERIPHERAL_BURST_TRANSFER_SINGLE,
+      dma::stream::cr::mburst::MEMORY_BURST_TRANSFER_SINGLE,
+      dma::stream::cr::chsel::CHANNEL_4);
 
   DMA_U1TX::setNumberOfTransactions(sizeof(msg));
 
   DMA_U1TX::setMemory0Address(&msg);
-  DMA_U1TX::setPeripheralAddress(&_USART1->DR);
+  DMA_U1TX::setPeripheralAddress(&USART1_REGS->DR);
 
-  DMA_U1TX::enable();
+  DMA_U1TX::enablePeripheral();
 
   while (true) {
   }
