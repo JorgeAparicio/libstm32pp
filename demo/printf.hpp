f90ee69cdab79b95dea2aad048e500b33f08ef86
Jorge Aparicio <jorge.aparicio.r@gmail.com>
2012-08-29 00:53:20 -0500

    Added printf demo.

---
+++ demo/printf.hpp
@@ -0,0 +1,90 @@
+/*******************************************************************************
+ *
+ * Copyright (C) 2012 Jorge Aparicio <jorge.aparicio.r@gmail.com>
+ *
+ * This file is part of libstm32pp.
+ *
+ * libstm32pp is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * libstm32pp is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libstm32pp. If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+#pragma once
+
+// DO NOT INCLUDE THIS FILE ANYWHERE. THIS DEMO IS JUST A REFERENCE TO BE USED
+// IN YOUR MAIN SOURCE FILE.
+
+/**
+ * IMPORTANT: Include system_call_cpp.hpp in a source file once, this will
+ *            define the newlib stubs necessary for new, delete, printf, etc.
+ *
+ *            Also go to sytem_call.hpp and define which USART port will be
+ *            used for stdout.
+ */
+#include <stdio.h>
+#include "peripheral/gpio.hpp"
+#include "peripheral/usart.hpp"
+
+typedef USART6 USART;
+typedef PC6 TX;
+typedef PC7 RX;
+
+int main()
+{
+  GPIOC::enableClock();
+  TX::setAlternateFunction(gpio::afr::USART4_6);
+  TX::setMode(gpio::moder::ALTERNATE);
+  RX::setAlternateFunction(gpio::afr::USART4_6);
+  RX::setMode(gpio::moder::ALTERNATE);
+
+  USART::enableClock();
+  USART::configure(
+      usart::cr1::rwu::RECEIVER_IN_ACTIVE_MODE,
+      usart::cr1::re::RECEIVER_ENABLED,
+      usart::cr1::te::TRANSMITTER_ENABLED,
+      usart::cr1::idleie::IDLE_INTERRUPT_DISABLED,
+      usart::cr1::rxneie::RXNE_ORE_INTERRUPT_DISABLED,
+      usart::cr1::tcie::TC_INTERRUPT_DISABLED,
+      usart::cr1::txeie::TXEIE_INTERRUPT_DISABLED,
+      usart::cr1::peie::PEIE_INTERRUPT_DISABLED,
+      usart::cr1::ps::EVEN_PARITY,
+      usart::cr1::pce::PARITY_CONTROL_DISABLED,
+      usart::cr1::wake::WAKE_ON_IDLE_LINE,
+      usart::cr1::m::START_8_DATA_N_STOP,
+      usart::cr1::ue::USART_ENABLED,
+      usart::cr1::over8::OVERSAMPLING_BY_16,
+      usart::cr2::stop::_1_STOP_BIT,
+      usart::cr3::eie::ERROR_INTERRUPT_DISABLED,
+      usart::cr3::hdsel::FULL_DUPLEX,
+      usart::cr3::dmar::RECEIVER_DMA_DISABLED,
+      usart::cr3::dmat::TRANSMITTER_DMA_DISABLED,
+      usart::cr3::rtse::RTS_HARDWARE_FLOW_DISABLED,
+      usart::cr3::ctse::CTS_HARDWARE_FLOW_DISABLED,
+      usart::cr3::ctsie::CTS_INTERRUPT_DISABLED,
+      usart::cr3::onebit::ONE_SAMPLE_BIT_METHOD);
+
+  USART::setBaudRate<
+      921600 /* bps */
+  >();
+
+  /**
+   * IMPORTANT: printf employs a buffer under the hood, and only writes to
+   *            stdout when a '\n' (newline) character is spotted.
+   */
+//  printf("Hello World!"); // This will not print
+  printf("Hello World!\n"); // This will print
+
+  while (true) {
+
+  }
+}
